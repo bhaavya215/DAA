ab302d879b9a4f5b01e48a638f357862f5060e0b
Bhaavya Srivastava <srivastava.bhaavya02@gmail.com>
2025-02-20 22:10:45 +0530

    added a file

---
+++ duplicate.c
@@ -0,0 +1,3 @@
+/*Given a sorted array of positive integers containing few duplicate elements, design an algorithm & 
+implement it using a program to find whether the given key element is present in the array or not. 
+If present, then also find the number of copies of given key.*/
